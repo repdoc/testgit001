ee927b41de0068c92e349d94467dd7f0c4c73bfe
repdoc <narj.pol@gmail.com>
2022-09-06 10:31:44 +0200

    commit numero 2

--- main.java
+++ main.java
@@ -1,5 +1,9 @@
 public class Main{
     public static void main(String args[]){
         System.out.println("Hello from Main");
+    Pangolin p = new Pangolin("acd",10);
+    System.out.println(p.crier());
+    
     }
+
 }
