2eb11be847ecd12dc8d734bdc265dc14a8099150
repdoc <narj.pol@gmail.com>
2022-09-06 11:18:03 +0200

    Revert "renomer tata revert"
    This reverts commit e824685d511281f9f9b66e47e999d4310695b451.

--- main.java
+++ main.java
@@ -1,7 +1,7 @@
 public class Main{
     public static void main(String args[]){
         System.out.println("Hello from Main");
-    Pangolin p = new Pangolin("tata",10);
+    Pangolin p = new Pangolin("acd",10);
     System.out.println(p.crier());
     
     }
